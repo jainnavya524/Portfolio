bff2335a093af2f1004103636b3edbb8ec334792
Navya Jain <jainnavya524@gmail.com>
2026-05-11 16:25:39 +0530

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,37 @@
+const PROJECTS = [
+  {
+    id: 1,
+    title: "Github Profile README",
+    emoji: "📜",
+    shortDesc: "A personalized GitHub profile README showcasing my skills, learning journey, and tech interests.",
+    techStack: ["Markdown", "GitHub", "HTML", "CSS"],
+    longDesc:
+      "This GitHub README is a personalized developer profile designed to showcase my introduction, technical interests, learning journey, and online presence in a creative and visually appealing way. It highlights my passion for technology, coding, and continuous learning while giving visitors a quick overview of who I am as a CSE student and aspiring developer.",
+    features: [
+      "Personal introduction section with profile banner",
+      "Showcases technical interests and learning journey",
+      "Includes social and professional links",
+      "Clean and visually appealing layout",
+      "Responsive and beginner-friendly design",
+    ],
+    githubLink: "https://github.com/jainnavya524/jainnavya524",
+    status: "Completed",
+  }
+
+  // -------------------------------------------------------
+  // TO ADD NEW PROJECT COPY-PASTE HERE:
+  // {
+  //   id: 3,
+  //   title: "Project Name",
+  //   emoji: "🚀",
+  //   shortDesc: "In one line that what's in the project.",
+  //   techStack: ["React", "Node.js"],
+  //   longDesc: "Write complete details here",
+  //   features: [
+  //     "Feature 1",
+  //     "Feature 2",
+  //   ],
+  //   githubLink: "https://github.com/AnshKaushik9/your-repo",
+  //   status: "Completed", or "In Progress"
+  // },
+];
